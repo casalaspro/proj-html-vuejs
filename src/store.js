36751f03b09821fe7c19c2a7d8f81df2bef448cb
casalaspro <casalaspro.alessandro@gmail.com>
2024-04-22 17:03:49 +0200

    ADD - skeleton with scss components

---
+++ src/store.js
@@ -0,0 +1,120 @@
+import { reactive } from "vue";
+
+export const store = reactive({
+  muenuHeader:[
+    {
+      name: 'Home',
+      link: ''
+    },
+    {
+      name: 'Shop',
+      link: ''
+    },
+    {
+      name: 'Blog',
+      link: ''
+    },
+    {
+      name: 'Media',
+      link: ''
+    },
+    {
+      name: 'ShortCode',
+      link: ''
+    },
+    {
+      name: 'Features',
+      link: ''
+    }
+],
+  menuFooter: [{
+    addressInfo: [
+      {
+        address: "60 29th Street San Francisco, CA 94110 507-Union Trade Center, United States of América",
+        iconName: ''
+      },
+      {
+        telephone: "(+00) 123-456-789",
+        iconName: ''
+      },
+      {
+        email: "demo@example.com",
+        iconName: ''
+      }
+    ],
+    informations:[
+      {
+        name: 'Bookmark',
+        link: '#'
+      },
+      {
+        name: 'Features',
+        link: '#'
+      },
+      {
+        name: 'Sitemap',
+        link: '#'
+      },
+      {
+        name: 'Lists',
+        link: '#'
+      },
+      {
+        name: 'Contact Us',
+        link: '#'
+      }
+    ],
+    extras: [
+      {
+        name: 'Delivery',
+        link: '#'
+      },
+      {
+        name: 'Cart',
+        link: '#'
+      },
+      {
+        name: 'Terms conditions',
+        link: '#'
+      },
+      {
+        name: 'My acount',
+        link: '#'
+      },
+      {
+        name: 'About us',
+        link: '#'
+      }
+    ],
+    acceptedCards: [
+      {
+        name: 'American Express',
+        iconName: '',
+        link: ''
+      },
+      {
+        name: 'American Express',
+        iconName: '',
+        link: ''
+      },
+      {
+        name: 'American Express',
+        iconName: '',
+        link: ''
+      },
+      {
+        name: 'American Express',
+        iconName: '',
+        link: ''
+      },
+      {
+        name: 'American Express',
+        iconName: '',
+        link: ''
+      }
+    ]
+
+
+  }]
+
+})
